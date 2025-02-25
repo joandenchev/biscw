2221177761d4f934a01a36d9d21cb13a2f304b20
Joan <joandenchev@gmail.com>
2025-02-26 00:57:46 +0200

    Resizable tabs

--- src/Components/Global.vue
+++ src/Components/Global.vue
@@ -1,21 +1,66 @@
+<!--suppress JSUnusedGlobalSymbols -->
 <script setup lang="js">
 import RightTab from "./RightTab.vue";
 import LeftTab from "./LeftTab.vue";
-import {computed} from "vue";
+import {computed, onMounted, reactive, ref} from "vue";
 
-let ltW = 49.92
-let rtW = 49.92
+const ltW = ref(49.92)
+const rtW = ref(49.92)
+const splitHovered = ref(false)
+const resizing = ref(false)
 
-let ltWC = computed(() => ltW + '%')
-let rtWC = computed(() => rtW + '%')
+const ltWC = computed(() => ltW.value + '%')
+const rtWC = computed(() => rtW.value + '%')
+const isWC = computed(() => resizing.value ? '20%' : '0.48%')
+const cursor = computed(() => resizing.value ? 'grabbing' : 'grab')
+
+function resize(event){
+  if (resizing.value){
+    ltW.value = event.clientX / window.innerWidth * 100
+    rtW.value = 99.84 - ltW.value
+  }
+}
+function touchResize(event){
+  if (resizing.value){
+    console.log('hey touchyyy')
+    ltW.value = event.touches[0].clientX / window.innerWidth * 100
+    rtW.value = 99.84 - ltW.value
+  }
+}
+
+const touchControlSplit = reactive({})
+const touchControlIncrease = reactive({})
+
+
+onMounted(()=>{
+  if('ontouchstart' in window || navigator.maxTouchPoints > 0){
+    touchControlSplit.touchstart=()=>{splitHovered.value=true}
+    touchControlIncrease.touchstart=()=>{resizing.value=true}
+    touchControlIncrease.touchmove=touchResize
+    touchControlIncrease.touchend=()=>{resizing.value=false}
+  }
+})
 
 </script>
 
 <template>
   <left-tab></left-tab>
-  <div id="splitter"></div>
+  <div id="splitter"
+       v-on="touchControlSplit"
+       @mouseenter="splitHovered=true"
+  >
+    <div id="increaseSplitter"
+         v-if="splitHovered"
+         v-on="touchControlIncrease"
+         @mouseleave="resizing ? null : splitHovered=false"
+         @mousedown="resizing = true"
+         @mousemove="resize"
+         @mouseup  ="resizing = false"
+    >
+    </div>
+  </div>
+
   <right-tab></right-tab>
-  <div id="increaseSplitter"></div>
 </template>
 
 <style lang="scss">
@@ -25,6 +70,7 @@ html, body, #start{
   width: 100%;
   margin: 0;
   padding: 0;
+  overflow: hidden;
 }
 #start{
   display: flex;
@@ -35,11 +81,11 @@ html, body, #start{
 }
 #increaseSplitter{
   height: 100%;
-  width: 0.48%;
+  width: v-bind(isWC);
   min-width: 10px;
   position: absolute;
-  left: 50%;
   transform: translateX(-50%);
+  cursor: v-bind(cursor);
 }
 #lt {
   background-color: cadetblue;
